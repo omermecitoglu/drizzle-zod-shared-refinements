3684a4a8cee49a364986bebaecf200797c62d1bd
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2026-01-17 09:51:36 +0700

    feat: strictly typed refinement keys
    BREAKING CHANGE: keys of refinements will be strictly typed now
    
    Fixes #1

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import type { BuildRefine } from "drizzle-zod";
 
 export function createSharedRefinements<
   T extends Table,
-  R extends BuildRefine<Pick<T["_"]["columns"], keyof T["$inferInsert"]>, undefined>,
->(table: T, refinements: R): R {
+  ValidKeys extends keyof T["$inferInsert"],
+  R extends BuildRefine<Pick<T["_"]["columns"], ValidKeys>, undefined>,
+>(
+  table: T,
+  refinements: { [K in keyof R]: K extends ValidKeys ? R[K] : never },
+): R {
   return refinements;
 }
