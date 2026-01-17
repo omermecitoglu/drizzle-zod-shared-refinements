51fc7455f13e7c0ea9c5aec379a8f858eb6c5c80
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2026-01-17 09:41:50 +0700

    test: update tests with a real table

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,27 @@
+import { sql } from "drizzle-orm";
+import { index } from "drizzle-orm/pg-core";
+import { timestamp, uuid, varchar } from "drizzle-orm/pg-core/columns";
+import { pgTable } from "drizzle-orm/pg-core/table";
 import { describe, expect, it } from "vitest";
 import { createSharedRefinements } from "./index";
-import type { Table } from "drizzle-orm";
 
 describe("createSharedRefinements", () => {
+  const mockTable = pgTable("users", {
+    id: uuid().defaultRandom().primaryKey(),
+    name: varchar().notNull(),
+    email: varchar().notNull().unique(),
+    password: varchar({ length: 72 }).notNull(),
+    createdAt: timestamp({ withTimezone: true, precision: 3 }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+    updatedAt: timestamp({ withTimezone: true, precision: 3 }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+  }, table => [
+    index().on(table.createdAt),
+  ]);
+
   it("should be a function", () => {
     expect(typeof createSharedRefinements).toBe("function");
   });
 
   it("should return the second parameter", () => {
-    const mockTable = {} as Table;
     const refinements = {};
     const output = createSharedRefinements(mockTable, refinements);
     expect(output).toBe(refinements);
